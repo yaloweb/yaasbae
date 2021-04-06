e4d7b4753448aa885defd4e04222c7715286151e
Ravil <whiteravil@yandex.ru>
2021-04-06 15:59:25 +0300

    цветовая палитра сайта

--- app/js/common.js
+++ app/js/common.js
@@ -961,4 +961,27 @@ $(function() {
     });
   });
 
+  // Конвертация лого из <img /> в <svg />
+  const convertImages = (query, callback) => {
+    const images = document.querySelectorAll(query);
+
+    images.forEach(image => {
+      fetch(image.src)
+      .then(res => res.text())
+      .then(data => {
+        const parser = new DOMParser();
+        const svg = parser.parseFromString(data, 'image/svg+xml').querySelector('svg');
+
+        if (image.id) svg.id = image.id;
+        if (image.className) svg.classList = image.classList;
+
+        image.parentNode.replaceChild(svg, image);
+      })
+      .then(callback)
+      .catch(error => console.error(error))
+    });
+  }
+
+  convertImages('.header-logo .logo img');
+
 });
